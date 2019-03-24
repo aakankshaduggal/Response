548f6d72724b3ead4b92350a8190c0734bb08c37
lipim12 <lipi.mehta.12@gmail.com>
2019-03-24 03:23:43 -0400

    Fixing final

--- lipi_edits.py
+++ lipi_edits.py
@@ -7,15 +7,8 @@
 import csv
 import random
 
-with open('affirmationdata.csv') as f:
-    reader = csv.reader(f)
-    l = list(reader)
-    chosen_row1 = random.choice(l)[1]
-    chosen_row2 = random.choice(l)[1]
-    chosen_row3 = random.choice(l)[1]
     
 neutral = ["Okay","Alright","Good going", "Tell me more", "Sounds good", "Great", "Umhmm", "You gotta do what you gotta do"]
-
 
 
 """input data
@@ -23,6 +16,14 @@ neutral = ["Okay","Alright","Good going", "Tell me more", "Sounds good", "Great"
     sentiment between -1 to 1
 """
 def generatedRes(persona,sentiment):
+    with open('affirmationdata.csv') as f:
+    		reader = csv.reader(f)
+    		l = list(reader)
+    		chosen_row1 = random.choice(l)[1]
+    		chosen_row2 = random.choice(l)[1]
+    		chosen_row3 = random.choice(l)[1]
+
+		
     if not(sentiment==0):
         if(persona==1):
             return chosen_row1+". "+chosen_row2+". "+chosen_row3
